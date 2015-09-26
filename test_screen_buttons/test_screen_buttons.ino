79dafe25c233ec819ba4be70bd0672b635003251
Alphabasic <Samuel.a.hardy@gmail.com>
2015-09-25 20:05:56 -0400

    add doots

--- test_screen_buttons/test_screen_buttons.ino
+++ test_screen_buttons/test_screen_buttons.ino
@@ -1,22 +1,14 @@
 //Sample using LiquidCrystal library
 #include <LiquidCrystal.h>
 
-/*******************************************************
-
-This program will test the LCD panel and the buttons
-Mark Bramwell, July 2010
-
-********************************************************/
-
 // select the pins used on the LCD panel
 LiquidCrystal lcd(14, 15, 4, 5, 6, 7);
-
 
 /***************************************/
 const int motorIn1 = 20;  //attach to one of the pin of the motor
 const int motorIn2 = 21;  //attach to another pin of the motor
+const int speakerOut = 19; //piezo speaker
 /***************************************/
-
 
 // define some values used by the panel and buttons
 int lcd_key     = 0;
@@ -28,17 +20,23 @@ int adc_key_in  = 0;
 #define btnSELECT 4
 #define btnNONE   5
 
-// define motor controls
-int potPin = 23;
-int motorPin = 22;
-int potValue = 0;
-int motorValue = 0;
+//define piezo notes
+#define  c     3830    // 261 Hz 
+#define  d     3400    // 294 Hz 
+#define  e     3038    // 329 Hz 
+#define  f     2864    // 349 Hz 
+#define  g     2550    // 392 Hz 
+#define  a     2272    // 440 Hz 
+#define  b     2028    // 493 Hz 
+#define  C     1912    // 523 Hz 
+// Define a special note, 'R', to represent a rest
+#define  R     0
 
 // read the buttons
 int read_LCD_buttons()
 {
  adc_key_in = analogRead(16);      // read the value from the sensor 
- Serial.println(adc_key_in, DEC);
+ //Serial.println(adc_key_in, DEC);
  // my buttons when read are centered at these values: 0, 144, 329, 504, 741
  // we add approx 50 to those values and check to see if we are close
  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
@@ -54,18 +52,75 @@ int read_LCD_buttons()
 
 void setup()
 {
+ pinMode(speakerOut, OUTPUT);
  lcd.begin(16, 2);              // start the library
  lcd.setCursor(0,0);
  lcd.print("Welcome to..."); // print a simple message
- delay(1000);
+ delay(500);
  lcd.setCursor(0,0);
  lcd.print("Mr. Skeltal's");
  lcd.setCursor(0,1);
  lcd.print("Spooky Adventure!");
- delay(2000);
- 
+ Serial.begin(9600);
+ delay(1000);
  }
  
+ // MELODY and TIMING  =======================================
+//  melody[] is an array of notes, accompanied by beats[], 
+//  which sets each note's relative length (higher #, longer note) 
+int melody[] = {  C,  b,  g,  C,  b,   e,  R,  C,  c,  g, a, C };
+int beats[]  = { 16, 16, 16,  8,  8,  16, 32, 16, 16, 16, 8, 8 }; 
+//int MAX_COUNT = sizeof(melody); // Melody length, for looping.
+int MAX_COUNT = 13;
+int lengthOfMelody = sizeof(melody);
+// Set overall tempo
+long tempo = 10000;
+// Set length of pause between notes
+int pause = 1500;
+
+// Initialize core variables
+int tone_ = 0;
+int beat = 0;
+int currentStep = 0;
+long duration  = 0;
+
+// PLAY TONE  ==============================================
+// Pulse the speaker to play a tone for a particular duration
+void playTone() {
+  long elapsed_time = 0;
+  if (tone_ > 0) { // if this isn't a Rest beat, while the tone has 
+    //  played less long than 'duration', pulse speaker HIGH and LOW
+    while (elapsed_time < duration) {
+
+      digitalWrite(speakerOut,HIGH);
+      delayMicroseconds(tone_ / 2);
+
+      // DOWN
+      digitalWrite(speakerOut, LOW);
+      delayMicroseconds(tone_ / 2);
+
+      // Keep track of how long we pulsed
+      elapsed_time += (tone_);
+    } 
+  }
+  else {
+      delayMicroseconds(duration);                               
+  }                                 
+}
+void playSong(){
+   for (int i=0; i<MAX_COUNT; i++) 
+     {
+     currentStep = i%lengthOfMelody;
+     tone_ = melody[currentStep];
+     beat = beats[currentStep];
+
+     duration = beat * tempo; // Set up timing
+
+     playTone(); 
+     // A pause between notes...
+     delayMicroseconds(pause);
+    }
+};
  void clockwise(int Speed)
 {
   analogWrite(motorIn1,Speed);  //set the speed of motor
@@ -93,11 +148,11 @@ void ramp()
    lcd.print("Ramping up...  ");
    lcd.setCursor(0,1);
    lcd.print("               ");
-   for(int i=95; i < 125; i++){
+   for(int i=125; i < 250; i++){
      clockwise(i);
      lcd.setCursor(0,1);
      lcd.print(i);
-     delay(250); 
+     delay(150); 
    }
 }
  
@@ -115,12 +170,13 @@ void loop()
        case btnRIGHT:
          {
            ramp();
-           waitAndShow(30);
+           waitAndShow(5);
            clockwise(0);
            lcd.setCursor(0,0); 
            lcd.print("Stopped        ");
            lcd.setCursor(0,1);
            lcd.print("               ");
+           break;
          }
        case btnLEFT:
          {
@@ -140,8 +196,8 @@ void loop()
          //--------****************---
          lcd.setCursor(0,1);
          lcd.print("calcium! doot!  ");
-         //---------****************---
-         delay(3000);
+         //---------****************--
+         playSong();
          break;
          }
        case btnDOWN:
